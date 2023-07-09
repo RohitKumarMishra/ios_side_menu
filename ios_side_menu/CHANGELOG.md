56120ea3f403b9bbc3a35af22622bb163badbb0e
RohitKumarMishra <rohitkumar5@ymail.com>
2023-07-09 17:30:14 +0530

    resolved pub dev issues

--- ios_side_menu/CHANGELOG.md
+++ ios_side_menu/CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.1
+## 1.0.0
 
-* TODO: Describe initial release.
+* TODO: iOS side menu with native style animation and all edge cases handled for both android and ios.
--- ios_side_menu/test/ios_side_menu_test.dart
+++ ios_side_menu/test/ios_side_menu_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ios_side_menu/ios_side_menu.dart';
-
 void main() {
   test('adds one to input values', () {});
 }
